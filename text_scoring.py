3443454216a59a81f8500edc708f7046db236f2b
Jingbo Wang <jingbowang@lawn-128-61-12-86.lawn.gatech.edu>
2020-02-26 20:35:46 -0500

    first commit

---
+++ text_scoring.py
@@ -0,0 +1,45 @@
+# https://towardsdatascience.com/recurrent-neural-networks-by-example-in-python-ffd204f99470
+# https://www.tensorflow.org/tutorials/text/text_classification_rnn
+from keras.preprocessing.text import Tokenizer
+import tensorflow as tf
+import numpy as np
+
+features = ['I like dancing', 'BTS is kpop group']
+labels = [1, 0]
+
+# Create Tokenizer Object
+tokenizer = Tokenizer(
+	num_words=1000,
+	filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+	lower=True,
+	split=' ')
+
+# Train the tokenizer to the texts
+tokenizer.fit_on_texts(features)
+
+# Convert list of strings into list of lists of integers
+sequences = tokenizer.texts_to_sequences(features)
+padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding='post')
+
+# building RNN
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(tokenizer.num_words, 2),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(
+	loss=tf.keras.losses.MeanSquaredError(),
+	optimizer=tf.keras.optimizers.Adam(1e-4),
+	metrics=['mae'])
+
+# train the model
+print(np.array(padded_sequences))
+print(np.array(labels))
+history = model.fit(
+	x=np.array(padded_sequences),
+	y=np.array(labels),
+	epochs=10)
+
+
